a3c43772bdc829a9e8c90dda0a222efd64df3104
Yuriy Shustykevych <yuriy.shustykevych@gmail.com>
2024-01-02 04:47:58 +0200

    delete test nats

--- nats-test/src/events/tickets-created-publisher.ts
+++
@@ -1,9 +0,0 @@
-import {Publisher} from "./base-publisher";
-import {TicketCreatedEvent} from "./ticket-created-event";
-import {Subjects} from "./subjects";
-
-export class TicketsCreatedPublisher extends Publisher<TicketCreatedEvent>{
-    subject: Subjects.TicketCreated = Subjects.TicketCreated;
-
-
-}
