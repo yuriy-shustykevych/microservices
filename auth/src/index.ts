8e248da274e9fd5740d405191214f07990cfd096
Yuriy Shustykevych <yuriy.shustykevych@gmail.com>
2024-01-10 02:14:02 +0200

    pushing depl second

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 import {app} from "./app";
 
 const start = async ()=>{
-    console.log("Testing start depl!")
+    console.log("Testing start depl second")
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY must be defined")
     }
